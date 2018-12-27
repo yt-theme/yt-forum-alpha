8ad1b19c0206ce4d28bba8350fc6f3f329b41af5
a <131>
2018-12-27 15:42:49 +0800

    asf

--- src/components/indexPage/body/bodyCompo/body_popPub.vue
+++ src/components/indexPage/body/bodyCompo/body_popPub.vue
@@ -17,8 +17,12 @@
         <input ref="popPub_input_file" @change="selectFile()" style="width: 4.5em; opacity: 0;" type="file" name="file" class="pos_a bor_ra_3 bor_0 bgc_ligh_gray marg_0">
         <button class="bor_ra_3 top_0 bor_0 padd_4 bgc_ligh_gray bor_wh">选择文件</button>
       </span>
-      <span>{{selectedFile_prop.name}}</span>&nbsp;
-      <span>{{selectedFile_prop_size}}</span>
+      <span style="top: 6px;" class="pos_r over_h inl_block h_100">
+        <span>{{selectedFile_prop.name}}</span>&nbsp;
+        <span>{{selectedFile_prop_size}}</span>
+        <!-- 上传时效果 -->
+        <span v-if="uploadingStat" class="body_popPub_uploadStyle pos_a bottom_0"></span>
+      </span>
       <template v-if="selectedFile_prop">
         <button @click="addFile()" class="bor_ra_3 top_0 bor_0 padd_4 bgc_ligh_gray bor_wh">添加</button>
         <button @click="clearSelectedFile()" class="bor_ra_3 top_0 bor_0 padd_4 bgc_ligh_gray bor_wh">清除</button>
@@ -43,6 +47,8 @@ export default {
       selectedType: "web",
       // 已选文件对象
       selectedFile_prop: '',
+      // 正在上传状态
+      uploadingStat: false,
     }
   },
   computed: {
@@ -50,11 +56,11 @@ export default {
       if (this.selectedFile_prop) {
         let size = Number(this.selectedFile_prop.size)
         if (size>0 && size<1000) {
-          return size + 'b'
+          return '( ' + size + 'b )'
         } else if (size>1000 && size<1000000) {
-          return (size / 1000).toFixed(2) + 'kb'
+          return '( ' + (size / 1000).toFixed(2) + 'kb )'
         } else if (size>1000000) {
-          return (size / 1000 /1000).toFixed(2) + 'mb'
+          return '( ' + (size / 1000 /1000).toFixed(2) + 'mb )'
         }
       } else {
         return ''
@@ -76,13 +82,14 @@ export default {
     },
     // 向后端添加文件
     addFile () {
-      alert(111)
+      this.uploadingStat = true
     },
     // 清除已选择文件
     clearSelectedFile () {
       this.$refs.popPub_input_file.value = ''
       this.selectedFile_prop = ''
-    }
+      this.uploadingStat = false
+    },
   }
 }
 </script>
@@ -103,6 +110,17 @@ export default {
 }
 .body_popPub_inner textarea {
   height: calc(100% - 2em - 80px);
+}
+@keyframes uploadStyle {
+  0% { left: 0; }
+  100% { left: 100%; }
+}
+.body_popPub_uploadStyle {
+  display:inline-block;
+  height: 2px;
+  width: 30px;
+  box-shadow: 0 0 17px #f1f1f3 inset;
+  animation: uploadStyle 1s infinite;
 }
 </style>
 
